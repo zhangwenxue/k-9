721f4e3ffcc3de23fdb19f8e71a9b882c4c42540
zhangwenxue <1257545592@qq.com>
2018-02-27 16:44:08 +0800

    add material-design-icons

---
+++ primail/src/main/java/com/cm/primail/AddAccountsActivity.java
@@ -0,0 +1,13 @@
+package com.cm.primail;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddAccountsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_accounts);
+    }
+}
